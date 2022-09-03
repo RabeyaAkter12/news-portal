3c983b6b0595e697f6d730beb1de340edae0bfab
Rabeyakter <rabeyaakter7744@gmail.com>
2022-09-04 04:45:24 +0600

    2nd commit my code

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const loadDataAll = async () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`
+
+  try {
+    const res = await fetch(url)
+    const data = await res.json()
+    displayData(data.data.news_category);
+  }
+  catch (error) {
+    alert('The answer is not correct')
+  }
+}
+
+const displayData = allCategoryNews => {
+
+  const categoryPlace = document.getElementById('navbar-category')
+  allCategoryNews.forEach(news => {
+
+    const newsDiv = document.createElement('newsDiv')
+    newsDiv.innerHTML = `
+       <a id="show-news-length" onclick="showAllDetails('${news.category_id}')" class="nav-link" href="#">${news.category_name}</a>
+       `;
+    categoryPlace.appendChild(newsDiv)
+  });
+}
+
