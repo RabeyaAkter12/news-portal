0ded6d3a0c2ada26bf1691f059f4015a402c1b04
Rabeyakter <rabeyaakter7744@gmail.com>
2022-09-04 04:55:07 +0600

    code commit

--- js/app.js
+++ js/app.js
@@ -38,3 +38,55 @@ const showAllDetails = async (id) => {
     alert('The answer is not correct')
   }
 }
+
+
+
+// <--------------- display detail ------------>
+
+const displayDetails = categoryId => {
+  categoryId.sort((a, b) => {
+    return b.total_view - a.total_view
+  })
+
+  const showNews = document.getElementById('show-news').innerText = categoryId.length
+  const placeCard = document.getElementById('cards-container')
+  placeCard.innerHTML = '';
+  categoryId.forEach(id => {
+    // console.log(id);
+    const cardsDiv = document.createElement('div')
+    cardsDiv.classList.add('col')
+    cardsDiv.innerHTML = `;
+        <div class="card mb-4" style="max-width: 980px;">
+  <div class="row g-0">
+    <div class="col-md-4 ml-8">
+      <img src="${id.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h5 class="card-title">${id.title}</h5>
+        <p class="card-text cards">${id.details}</p>
+        <div class="d-flex justify-content-around align-items-center">
+       <div> <img class="img-fluid img" src="${id.author.img}" alt="">  ${id.author.name ? id.author.name : "No found name"}</div>
+       <div> 
+      <p> <i class="fa-solid fa-eye"></i>
+      ${id.total_view ? id.total_view : '1.3M '}</p>
+       </div>
+       <div>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star-half-stroke"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        </div> 
+       <button onclick="detailModal('${id._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">  Details
+       <i class="fa-solid fa-chevron-right"></i></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+        `;
+    placeCard.appendChild(cardsDiv)
+  })
+  loadSpinnerBulen(false);
+}
